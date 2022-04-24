a4a57218fd551993820ea3ce5e75447e8190aefe
José Carlos <jotacfs2010@hotmail.com>
2022-04-24 12:36:16 -0300

    adicionando chave assimétrica para criar e validar tokens via JWT

--- src/main/java/com/jcmj/AuthorizationServerConfig.java
+++ src/main/java/com/jcmj/AuthorizationServerConfig.java
@@ -3,6 +3,7 @@ package com.jcmj;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -16,6 +17,7 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 import org.springframework.security.oauth2.provider.CompositeTokenGranter;
 import org.springframework.security.oauth2.provider.TokenGranter;
 import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 
 @Configuration
@@ -30,7 +32,7 @@ public class AuthorizationServerConfig  extends AuthorizationServerConfigurerAda
 	private UserDetailsService detailsService;	
 	
 	@Autowired
-	private RedisConnectionFactory connectionFactory;
+	//private RedisConnectionFactory connectionFactory;
 	
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
@@ -80,14 +82,23 @@ public class AuthorizationServerConfig  extends AuthorizationServerConfigurerAda
 		  .authenticationManager(authenticationManager)
 		  .userDetailsService(detailsService)
 		  .reuseRefreshTokens(false)
+		  .accessTokenConverter(jwtAccessTokenConverter())
 		  .tokenGranter(tokenGranter(endpoints));
 		 // .tokenStore(redisTokenStore());
 		
 	}
-	//conecção com banco de dados redis 
-	private TokenStore redisTokenStore() {
-		return new RedisTokenStore(connectionFactory);
+	@Bean
+	public JwtAccessTokenConverter jwtAccessTokenConverter() {
+		JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+		jwtAccessTokenConverter.setSigningKey("Doutrina12*");
+		
+		return jwtAccessTokenConverter;
 	}
+	//conecção com banco de dados redis 
+	/*
+	 * private TokenStore redisTokenStore() { return new
+	 * RedisTokenStore(connectionFactory); }
+	 */
 	/*
 	 * @Override public void configure(AuthorizationServerEndpointsConfigurer
 	 * endpoints) { endpoints.tokenGranter(tokenGranter(endpoints)); }
