30f057f81d3683c9c080c4829cc73d48d059e8af
José Carlos <jotacfs2010@hotmail.com>
2022-04-24 11:22:46 -0300

    configurando Redis para armazenar tokens da aplicação

--- src/main/java/com/jcmj/AuthorizationServerConfig.java
+++ src/main/java/com/jcmj/AuthorizationServerConfig.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,6 +15,8 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.CompositeTokenGranter;
 import org.springframework.security.oauth2.provider.TokenGranter;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
@@ -24,8 +27,10 @@ public class AuthorizationServerConfig  extends AuthorizationServerConfigurerAda
 	@Autowired
 	private AuthenticationManager authenticationManager;
 	@Autowired
-	private UserDetailsService detailsService;
+	private UserDetailsService detailsService;	
 	
+	@Autowired
+	private RedisConnectionFactory connectionFactory;
 	
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
@@ -75,8 +80,13 @@ public class AuthorizationServerConfig  extends AuthorizationServerConfigurerAda
 		  .authenticationManager(authenticationManager)
 		  .userDetailsService(detailsService)
 		  .reuseRefreshTokens(false)
-		  .tokenGranter(tokenGranter(endpoints));
+		  .tokenGranter(tokenGranter(endpoints))
+		  .tokenStore(redisTokenStore());
 		
+	}
+	//conecção com banco de dados redis 
+	private TokenStore redisTokenStore() {
+		return new RedisTokenStore(connectionFactory);
 	}
 	/*
 	 * @Override public void configure(AuthorizationServerEndpointsConfigurer
