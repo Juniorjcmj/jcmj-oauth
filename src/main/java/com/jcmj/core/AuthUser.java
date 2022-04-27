ecfdccbffc97a51b5173d1a793b565bf9850ae57
José Carlos <jotacfs2010@hotmail.com>
2022-04-26 21:24:39 -0300

    carregando as permissoes concedidas na autenticacao  dentro do token

--- src/main/java/com/jcmj/core/AuthUser.java
+++ src/main/java/com/jcmj/core/AuthUser.java
@@ -1,7 +1,8 @@
 package com.jcmj.core;
 
-import java.util.Collections;
+import java.util.Collection;
 
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 
 import com.jcmj.domain.Usuario;
@@ -15,8 +16,8 @@ public class AuthUser extends User{
 	private Long userId;
 	private String fullName;
 
-      public AuthUser(Usuario usuario) {
-		super(usuario.getLogin(),usuario.getSenha(),Collections.emptyList());
+      public AuthUser(Usuario usuario, Collection<? extends GrantedAuthority> authorities) {
+		super(usuario.getLogin(),usuario.getSenha(),authorities);
 		
 		this.userId = usuario.getId();
 		this.fullName = usuario.getNome();
